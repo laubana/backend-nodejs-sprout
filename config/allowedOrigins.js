628c590e82f35eabf5142e320fed8d737ed18e1d
yban <laubana@gmail.com>
2024-11-19 08:45:48 -0800

    init

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+// add the react url to allowed origins after deployment
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "https://frontend-sprout-fork.yuhwan-ban.link",
+];
+
+module.exports = allowedOrigins;
---
+++ server.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv");
+// require("./services/scheduler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+
+// this is to make the public static file accessible globally, ex. public/css/style.css can be called with css/styles.css
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/userRoutes"));
+app.use("/api", require("./routes/cropRoutes"));
+app.use("/api", require("./routes/cropEncylopediaRoutes"));
+app.use("/api", require("./routes/conditionRoutes"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+//this is executed once when the connection is successful
+mongoose.connection.once("open", () => {
+  console.log("Connected to Database");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
+//this is executed once when an error occurs
+mongoose.connection.on("error", (error) => {
+  console.error(error);
+});
+
+module.exports = app;
